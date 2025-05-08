74e54b1a0538f4834d3a4f94a6bce9f283a5f195
Andrew Katz <andrew.katz@hey.com>
2025-05-08 09:38:41 -0700

    Init commit

---
+++ gnome/gnome-settings.sh
@@ -0,0 +1,8 @@
+# Get the directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
+echo "setting wallpaper to ${SCRIPT_DIR}/wallpaper.jpg"
+gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR}/wallpaper.jpg"
+gsettings set org.gnome.desktop.background picture-options 'zoom'
+gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
