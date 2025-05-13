974ea9c2e998c96dc9630607e8072a278d36dc50
Andrew Katz <andrew.katz@hey.com>
2025-05-13 15:08:22 -0700

    Add auto-scaler

--- gnome/gnome-settings.sh
+++ gnome/gnome-settings.sh
@@ -7,3 +7,6 @@ gsettings set org.gnome.desktop.background picture-uri-dark "file://${SCRIPT_DIR
 gsettings set org.gnome.desktop.background picture-options 'zoom'
 gsettings set org.gnome.mutter experimental-features "['scale-monitor-framebuffer']"
 gsettings set org.gnome.desktop.peripherals.keyboard delay 250
+
+echo "setting up auto-scale"
+cp ${SCRIPT_DIR}/set-scale.desktop ~/.config/autostart/
